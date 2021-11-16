2562622eb1ccda6a1b830f8b12a36c9554f579e5
account1 <andre.ciornavei@gmail.com>
2021-11-16 09:14:50 -0300

    remove unused styles from scaffold float

--- src/components/Scaffold/styles.ts
+++ src/components/Scaffold/styles.ts
@@ -8,22 +8,5 @@ export default StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     position: 'absolute',
-  },
-  floatButton: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    flexDirection: "row",
-    overflow: 'hidden',
-    backgroundColor: "#000",
-    borderWidth: 1,
-    borderColor: "#000",
-    alignContent: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 0
-  },
-  floatButtonLabel: {
-    fontSize: 12
-  }
+  }  
 });
